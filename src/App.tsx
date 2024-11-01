1420a5d6c5c6083116cf1dfaa32d64fd863c868c
masudrwd <info.masud21@gmail.com>
2024-11-01 13:34:11 +0600

    ga4

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import { Container } from '@mui/material';
 import Navbar from './Components/Navbar';
 import NoticeBoard from './Components/NoticeBoard';
 import AccordianComponent from './Components/Accordian';
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+
+const TrackingId = 'G-VLJ78XP35R';
 
 function App() {
+   useEffect(() => {
+      ReactGA.initialize(TrackingId);
+      ReactGA.send({
+         hitType: 'pageview',
+         page: '/homepage',
+         title: 'homepage',
+      });
+   }, []);
    return (
       <>
          <Navbar />
